98411227dd0801a3ecca3ea492d0b77fd1c048f5
alessandro chinazzo <alessandro.chinazzo@gmail.com>
2023-09-05 12:21:44 +0200

    Italian mail translation

---
+++ core/components/GoogleAuthenticatorX/lexicon/it/default.inc.php
@@ -0,0 +1,68 @@
+<?php
+/* 
+  *  GoogleAuthenticatorX
+  * 
+  *  Copyright 2014 by Mina Gerges <mina@minagerges.com>
+  * 
+  *  GoogleAuthenticatorX is free software; you can redistribute it and/or modify it under the
+  *  terms of the GNU General Public License as published by the Free Software
+  *  Foundation; either version 2 of the License, or (at your option) any later
+  *  version.
+  * 
+  *  GoogleAuthenticatorX is distributed in the hope that it will be useful, but WITHOUT ANY
+  *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  *  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  * 
+  *  You should have received a copy of the GNU General Public License along with
+  *  WipeCache; if not, write to the Free Software Foundation, Inc., 59 Temple
+  *  Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+$_lang['GoogleAuthenticatorX.gax'] = 'GoogleAuthenticatorX';
+
+$_lang['gax'] = 'GoogleAuthenticatorX';
+$_lang['gax.desc'] = 'Add 2-step verification to MODX manager login.';
+$_lang['gax.description'] = 'Description';
+
+$_lang['setting_gax_disabled'] = 'Disable 2-factor authentication';
+$_lang['setting_gax_disabled_desc'] = 'Disabling 2-factor authentication will revert manager login back to default.';
+$_lang['setting_gax_courtesy_enabled'] = 'Allow Courtesy login';
+$_lang['setting_gax_courtesy_enabled_desc'] = 'Allowing courtesy login will enable users to have a 1 time login to retrieve their secret.';
+$_lang['setting_gax_profile_enabled'] = 'Show secret in users profile';
+$_lang['setting_gax_profile_enabled_desc'] = 'Allow users to see Google Authentication secret in their manager user profile.';
+$_lang['setting_gax_issuer'] = 'QR-code issuer value';
+$_lang['setting_gax_issuer_desc'] = 'Set the QR-code issuer value, default to site_name';
+$_lang['setting_gax_encrypt_key'] = 'Encryption key';
+$_lang['setting_gax_encrypt_key_desc'] = 'Encryption key used for 2FA data encryption. Do not change. Must match cipher method key requirement (64 HEX characters) subject to change.';
+
+$_lang['gax.authkey'] = 'Authentication Key';
+$_lang['gax.enterkey'] = 'Please enter Authentication key!';
+$_lang['gax.invalidformat'] = 'Invlid authentication key format, expecting 6 digits!';
+$_lang['gax.invalidcode'] = 'Invalid authentication key.';
+
+$_lang['gax.hello'] = 'Hello';
+$_lang['gax.usertab_desc'] = 'Here you can manage GoogleAuthenticatorX  <i>(Google Authenticator 2-factor authentication)</i> user specific tasks.';
+$_lang['gax.enabled'] = 'Enabled';
+$_lang['gax.disabled'] = 'Disabled';
+$_lang['gax.lbl_status'] = 'Status';
+$_lang['gax.lbl_secret'] = 'Secret';
+$_lang['gax.lbl_uri'] = 'URI';
+$_lang['gax.lbl_qrcode'] = 'QR-code';
+$_lang['gax.btn_showsecret'] = 'Show secret';
+$_lang['gax.btn_changestatus'] = 'Change Status';
+$_lang['gax.btn_changestatus_confirm'] = 'Are you sure you want to change Google Authenticator status for this user?';
+$_lang['gax.btn_enablegax'] = 'Enable Google Authenticator';
+$_lang['gax.btn_disablegax'] = 'Disable Google Authenticator';
+$_lang['gax.clkfrst'] = 'Please click "Show secret" first!';
+$_lang['gax.btn_resetsecret'] = 'Reset Secret';
+$_lang['gax.btn_resetsecret_confirm'] = 'Are you sure you want to reset Google Authenticator user secret? You will have to provide new key to user!';
+$_lang['gax.btn_emailinstruct'] = 'Email instructions to user';
+$_lang['gax.btn_emailinstruct_confirm'] = 'Are you sure you want to send instructions email to this user?';
+$_lang['gax.btn_emailqr'] = 'Email QR-code to user';
+$_lang['gax.btn_emailqr_confirm'] = 'Are you sure you want to email Google Authenticator secret to this user? This is the worst way to provide user secret!';
+
+$_lang['gax.courtesy_notification'] = 'This is a Courtesy login, follow the instructions provided by your administrator to be able to perform future login.';
+$_lang['gax.courtesy_qrfailed'] = 'Failed to retrieve QR-code';
+
+
+
+$_lang['gax.confirm_resetsecret'] = 'Are you sure you want to reset Google Authenticator user secret? You will have to provide the new key to user!';
---
+++ core/components/GoogleAuthenticatorX/lexicon/it/emailtpl.inc.php
@@ -0,0 +1,76 @@
+<?php
+/* 
+  *  GoogleAuthenticatorX
+  * 
+  *  Copyright 2014 by Mina Gerges <mina@minagerges.com>
+  * 
+  *  GoogleAuthenticatorX is free software; you can redistribute it and/or modify it under the
+  *  terms of the GNU General Public License as published by the Free Software
+  *  Foundation; either version 2 of the License, or (at your option) any later
+  *  version.
+  * 
+  *  GoogleAuthenticatorX is distributed in the hope that it will be useful, but WITHOUT ANY
+  *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  *  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  * 
+  *  You should have received a copy of the GNU General Public License along with
+  *  WipeCache; if not, write to the Free Software Foundation, Inc., 59 Temple
+  *  Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+$_lang['gax.emailsuccess'] = 'E-mail inviata con successo.';
+$_lang['gax.emailfail'] = 'Invio fallito.';
+
+$_lang['gax.qremail_subject'] = 'Il tuo QR-code Google Authenticator Modx';
+$_lang['gax.qremail_body'] = '<p>Ciao [[+username]],</p><p>Per utilizzare Google Authenticator hai bisogno di un dispositivo supportato, (Android, iPhone, BlackBerry).'
+. 'Per alcuni dispositivi potresti avere bisogno anche di un QR-code scanner. Apri Google Authenticator '
+. 'e fai la scansione del QR-code qui sotto; una volta completata la scansione con successo, vedrai una chiave di autenticazione che cambia ogni 30 secondi.'
+. 'Questa è la chiave richiesta al login.</p>'
+. '<br/> <img src="[[+qrurl]]"></body></html>';
+
+$_lang['gax.notifyemail_subject'] = 'E\' stata attivata l\'autenticazione a 2 fattori per il tuo account Modx';
+$_lang['gax.notifyemail_body'] = '<p>Ciao [[+username]],</p><p>Hai ricevuto questo messaggio perché è stata attivata l\'autenticazione a 2 fattori per il tuo account Modx. 
+		  Per utilizzare Google Authenticator hai bisogno di un dispositivo supportato, (Android, iPhone, BlackBerry). Per alcuni dispositivi potresti avere bisogno anche di un QR-code scanner.
+		  Avrai la possibilità di fare login senza chiave di autenticazione una sola volta; una volta fatto login, potrai fare la scansione del QR-code e verrai disconnesso.</p>
+		  <p>Apri Google Authenticator e fai la scansione del QR-code qui sotto; una volta completata la scansione con successo, vedrai una chiave di autenticazione che cambia ogni 30 secondi. Questa è la chiave richiesta quando fai login.</p>
+		  <p><br/><strong>NB. Il QR-code sara visibile solamente per 60 secondi.</strong></p><hr/>
+		  <div style="margin-top: 0px;">
+		  <h3>Istruzioni step by step</h3>
+		  <h4>Requisiti</h4>
+
+		  <p>Per utilizzare Google Authenticator sul tuo dispositivo Android, è necessaria una versione di Android uguale o maggiore della 2.1.</p>
+		  <p>Per utilizzare Google Authenticator sul tuo iPhone, iPod Touch, o iPad, è necessaria una versione di iOS uguale o maggiore della 5.0. Inoltre, per poter installare l\'app sul tuo iPhone utilizzando un QR code, è necessario avere un modello 3G o superiore.</p>
+		  <p>Per utilizzare Google Authenticator sul tuo dispositivo BlackBerry, è necessaria una versione OS compresa tra 4.5 e 7-0. Inoltre, assicurati che il tuo dispositivo BlackBerry sia configurato per Inglese US.</p>
+
+		  <h4>Scaricare l\'app</h4>
+
+		  <ol><strong>Per dispositivi Android:</strong>
+			<li>Vai su <a href="//play.google.com/">Google Play</a>.</li>
+			<li>Cerca <strong>Google Authenticator</strong>.</li>
+			<li>Scarica e installa l\'applicazione.</li>
+		  </ol>
+		  
+		  <ol><strong>Per iPhone, iPad e altri dispositivi iOS</strong>
+			<li>Visit l\'App Store.</li>
+			<li>Cerca <strong>Google Authenticator</strong>.</li>
+			<li>Scarica e installa l\'applicazione.</li>
+		  </ol>
+		  
+		  <ol><strong>Per dispositivi BlackBerry</strong>
+			<li>Apri il browser sul tuo BlackBerry.</li>
+			<li>Vai su m.google.com/authenticator.</li>
+			<li>Scarica e installa l\'applicazione.</li>
+		  </ol>
+
+		  <h4>Configurare l\'app</h4>
+
+		  <ol>
+			<li>Sul tuo dispositivo, apri Google Authenticator.</li>
+			<li>Se è la prima volta che utilizzi Authenticator, clicca il pulsante <strong>Add an account</strong>. Se stai aggiungendo un nuovo account, seleziona “Add an account” dal menu.</li>
+			<li>Per collegare il tuo dispositivo al tuo account:
+			  <ul>
+				<li><strong>Con un QR code</strong>: Seleziona <strong>Scan a QR code</strong>. Se Authenticator non riesce a trovare un barcode scanner app sul tuo dispositivo, ti potrebbe essere richiesto di installarne uno. Se desideri installare un barcode scanner app sul tuo dispositivo, clicca <strong>Install</strong> e concludi l\'installazione. Una volta che l\'app è installata, riapri Google Authenticator e inquadra il QR code sullo schermo del tuo computer.</li>
+				<li><strong>Con la secret key</strong>: Seleziona <strong>Enter a setup key</strong> e inserisci l\'indirizzo e-mail del tuo account Google nel campo <strong>Account</strong>. Successivamente, inserisci la secret key nel campo <strong>Key</strong>. Assicurati di aver selezionato di rendere la key <strong>Time based</strong> e clicca su "Add."</li>
+			  </ul>
+			  <br>
+		  </ol>
+		</div>';
